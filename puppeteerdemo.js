5788417378354694b8aa7fa076016731c7c525a7
Levi Ladd <levi.ladd.03@gmail.com>
2025-03-19 11:20:02 -0400

    maybe progress on launching opera w adblock?

--- puppeteerdemo.js
+++ puppeteerdemo.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
-import { testDailyWiki } from "./wiki-daily-test/wiki-test.js";
+import puppeteer from 'puppeteer';
+import { testFandom } from './fandom-test/fandom-test.js';
 
 const application = 'operagx';
 const map = {
@@ -7,21 +8,25 @@ const map = {
     'chrome': 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
     'firefox': 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
 }
-const exePath = map[application];
-fs.writeFileSync('wiki-daily-test/time.txt', "");
-for (const key in map) {
-    var avg = 0;
-    var arr = [];
-    for (var i = 0; i < 10; i++) {
-        const time = await testDailyWiki(key, map[key]);
-        arr.push(time);
-        avg += time;
-    }
-    avg /= 10.0;
+fs.writeFileSync('data/fandom/time.txt', "");
+console.log(puppeteer.defaultArgs());
+
+const time = await testFandom(application, map[application]);
+console.log(time);
+
+// for (const key in map) {
+//     var avg = 0;
+//     var arr = [];
+//     for (var i = 0; i < 10; i++) {
+//         const time = await testFandom(key, map[key]);
+//         arr.push(time);
+//         avg += time;
+//     }
+//     avg /= 10.0;
     
-    try {
-        const browserString = exePath.substring(exePath.lastIndexOf('\\')+1, exePath.length-4);
-        fs.appendFileSync('wiki-daily-test/time.txt', "Opera GX: " + avg + " ms | " + arr.toString());
-    } catch (error) {}
-}
+//     try {
+//         const browserString = exePath.substring(exePath.lastIndexOf('\\')+1, exePath.length-4);
+//         fs.appendFileSync('data/fandom/time.txt', key + ": " + avg + " ms | " + arr.toString());
+//     } catch (error) {}
+// }
 
